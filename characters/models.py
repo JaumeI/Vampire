954256d9ec907528f003d9c4f529aacd8c70bd16
JaumeI <jaumei@gmail.com>
2019-05-06 20:19:18 +0200

    proves bdd

--- characters/models.py
+++ characters/models.py
@@ -9,30 +9,13 @@ class Section(models.Model):
         return self.name
 
 class Entry(Section):
-    minLevel=0
-    maxLevel=10
-    #level = models.PositiveIntegerField(validators=[MinValueValidator(minLevel), MaxValueValidator(maxLevel)], default=1)
-    #models.ForeignKey('Level', blank=True, null=True, related_name="belongs_entry", on_delete=models.CASCADE)
-
     general_desc = models.TextField(blank=True)
     belongs_to = models.ForeignKey('self', blank=True, null=True, related_name="belongs_entry", on_delete=models.CASCADE)
 
 
-
-    def increase_level(self,increase):
-        self.level = self.level + increase
-        if self.level > self.maxLevel:
-            self.level= self.maxLevel
-
-    def decrease_level(self, decrease):
-        self.level = self.level - decrease
-        if self.level < self.minLevel:
-            self.level = self.minLevel
-
-
 class Level(Entry):
-    number = models.PositiveIntegerField()
+    lvl = models.PositiveIntegerField()
     system_desc = models.TextField(blank=True, null=True)
     system_attribute = models.ForeignKey('Entry', blank=True, null=True, on_delete='CASCADE', related_name='attribute')
     system_ability = models.ForeignKey('Entry', blank=True, null=True, on_delete='CASCADE', related_name='ability')
-    difficulty = models.PositiveIntegerField(blank=True, null=True,default=6)
+    difficulty = models.CharField(max_length=150, blank=True, null=True, default='6')
